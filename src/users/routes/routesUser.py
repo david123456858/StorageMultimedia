390488eead46ef578a1997a93e8ab157c45d2a5f
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-11 09:01:13 -0500

    cambiando las rutas

--- src/users/routes/routesUser.py
+++ src/users/routes/routesUser.py
@@ -6,12 +6,12 @@ route =  APIRouter(prefix='/user',tags=['users'])
 ## funcion que carga todas las rutas
 def moduleRouterUser () -> APIRouter:
     
-    @route.get('/')
+    @route.get('/auth/register')
     def getUser():
         return {'message':'esta listo la ruta de user' }
     
     
-    @route.get('/auth')
+    @route.get('/auth/login')
     def getAuth():
         return{'message':' ruta de autenticación'}
     return route
