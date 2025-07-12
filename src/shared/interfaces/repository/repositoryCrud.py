9138861b4ff437fa1a6027a53e52fbf0d6d99478
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-11 21:54:09 -0500

    comentarios

--- src/shared/interfaces/repository/repositoryCrud.py
+++ src/shared/interfaces/repository/repositoryCrud.py
@@ -4,6 +4,9 @@ from typing import TypeVar
 from src.shared.interfaces.repository.respositorySeach import ISearchRepository
 
 T = TypeVar('T')
+# clases abstractas y metodos abtractos 
+# decoradores
+
 
 class repositoryCrud(ISearchRepository[T]):
     
