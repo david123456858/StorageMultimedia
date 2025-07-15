115415e81833dadbc2c6f44ac98475a1cb039dcc
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-15 12:03:57 -0500

    instalacion de rust

--- src/config/db/db.py
+++ src/config/db/db.py
@@ -1,21 +1,16 @@
-from sqlalchemy import create_engine
-import os
-from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine
+# import os
+# from dotenv import load_dotenv
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv()
+# load_dotenv()
 
 
-DATA_BASE = f'sqlite+{os.getenv('MY_SQL')}?authToken={os.getenv('MY_TOKE_SQL')}'
-print(DATA_BASE)
-engine = create_engine(DATA_BASE)
-localSession = sessionmaker(autocommit=True, autoflush=True, bind=engine)
-Base = declarative_base() 
+# DATA_BASE = f'sqlite+{os.getenv('MY_SQL')}?authToken={os.getenv('MY_TOKE_SQL')}'
 
-try:
-    with engine.connect() as connection:
-        print('Se conecto')
-except:
-    print('❌ No se conecto')
-    
+# engine = create_engine(DATA_BASE)
+# localSession = sessionmaker(autocommit=True, autoflush=True, bind=engine)
+# Base = declarative_base() 
+
+
