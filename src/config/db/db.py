871b687c64fc1a5b73c95a63c4ffda3cabeda5d6
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-15 11:41:41 -0500

    tenemos que cambiar librerias

--- src/config/db/db.py
+++ src/config/db/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
 
-DATA_BASE = f'{os.getenv('MY_SQL')}?authToken={os.getenv('MY_TOKE_SQL')}'
+DATA_BASE = f'sqlite+{os.getenv('MY_SQL')}?authToken={os.getenv('MY_TOKE_SQL')}'
 print(DATA_BASE)
 engine = create_engine(DATA_BASE)
 localSession = sessionmaker(autocommit=True, autoflush=True, bind=engine)
