95b4ec28cf2701289aabbabf1e8fb952991984fc
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-23 18:11:20 -0500

    empezando login

--- src/users/caseuse/login.py
+++ src/users/caseuse/login.py
@@ -1,3 +1,14 @@
+from src.users.repository.user import repositoryUser
+from src.shared.utils.result import SuccessProccess,FailureProccess
 class caseUseUserLogin():
-    async def createLogin(self,userDto):
-        return { userDto }
+    
+    def __init__(self,repository:repositoryUser) -> None:
+        self. repo = repository
+        pass
+    def createLogin(self,userDto):
+        try:
+            
+
+            return SuccessProccess(200,'OK')
+        except Exception as e:
+            return FailureProccess(500,'Error internal sever')
