c0755e461b5bb446d4006adf4d09288c3b97feea
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-27 22:13:07 -0500

    solucionado

--- src/users/caseuse/login.py
+++ src/users/caseuse/login.py
@@ -1,8 +1,7 @@
 from src.users.repository.user import repositoryUser
 from src.shared.utils.result import SuccessProccess,FailureProccess
 from src.users.dtos.user import userDtoLogin
-from src.shared.utils.encrypt.encrypt import verify_encrypt
-
+from src.shared.utils.encrypt.hashed import verifyHashed
 class caseUseUserLogin():
     
     def __init__(self,repository:repositoryUser) -> None:
@@ -14,18 +13,16 @@ class caseUseUserLogin():
             # getattr(user_find,'password',None)
             # Obtiene lo que tiene de resultado un objeto en este caso
             # seria que va a scar user_find.password y lo convertira en un str 
-            # #
+            
             user_find = self.repo.find_by_email(userDto.email)
             password_hash = getattr(user_find,'password',None)
-            print(user_find)
-            print(password_hash)
+        
             if not isinstance(password_hash, str) or password_hash is None:
                 return FailureProccess(500, 'Contraseña inválida en base de datos')
             
-            result_verify = verify_encrypt(password_hash, userDto.password)
-            print(result_verify)
-            # if not result_verify:
-            #     FailureProccess(401,'Contraseña incorrecta')
+            result_verify = verifyHashed(userDto.password,password_hash)
+            if not result_verify:
+                FailureProccess(401,'Contraseña incorrecta')
             
             return SuccessProccess(200,'OK')
         except Exception as e:
