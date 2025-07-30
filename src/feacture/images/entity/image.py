ff8850111e7ba55192b4656248b80fd85cc89c5c
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-30 10:16:26 -0500

    eliminando carpetas innecesarias

--- src/feacture/images/entity/image.py
+++
@@ -1,9 +0,0 @@
-from src.config.db.db import Base
-from sqlalchemy import Column, Integer, String
-
-class Image(Base):
-    __tablename__ = 'images'
-    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    name = Column(String, nullable=False)
-    tag = Column (String, nullable=False) 
-    url = Column(String, nullable=False)
